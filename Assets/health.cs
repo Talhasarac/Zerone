b674954a007bb950769ba7c0bf94ccb166b27b51
Talhasarac <Talhasarac@users.noreply.github.com>
2019-05-11 04:52:36 +0300

    first upload

---
+++ Assets/health.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health : MonoBehaviour {
+
+
+	public float healthf;
+	float handhealth;
+	lookat lkat;
+	bool oldumoldum;
+	float hasarsizskengecensure;
+	public GameObject bumb;
+	// Use this for initialization
+	void Start () {
+		handhealth = healthf;
+		lkat = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<lookat> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hasarsizskengecensure += Time.deltaTime;
+		if (handhealth != healthf) {
+
+			hasarsizskengecensure = 0;
+			bool	 bv = (Random.Range (0, 2) == 1);
+			if(bv == true) {
+				this.transform.parent.GetComponent<Pai> ().attacking = false;
+			}
+
+
+			handhealth = healthf;
+
+		}
+		if (hasarsizskengecensure > 3f) {
+
+			this.transform.parent.GetComponent<Pai> ().attacking = true;
+			hasarsizskengecensure = 0;
+		}
+
+
+
+		if (healthf <= 0) {
+			this.GetComponent<BoxCollider> ().enabled = false;
+			if (oldumoldum == false) {
+				lkat.SendMessage ("awoker");
+				Instantiate (bumb, transform.parent.transform.position, transform.parent.transform.rotation);
+				oldumoldum = true;
+			}
+
+
+			Destroy (this.transform.parent.gameObject,0.2f);
+		
+		}
+	
+		
+	}
+
+	void OnTriggerEnter(Collider emi)
+	{
+		if (emi.tag == "bullet") {
+			if (emi.GetComponent<ammosc> ().whoami != this.transform.parent.tag) {
+				healthf -=	emi.GetComponent<ammosc> ().dmg;
+			}
+		}
+
+
+	}
+}
---
+++ Assets/sensorler.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sensorler : MonoBehaviour {
+
+	public bool bisivr;
+	BoxCollider bx;
+	public Vector3 nwsz;
+	public Vector3 nwcntr;
+	public Collider cd;
+	// Use this for initialization
+	void Start () {
+		bx = GetComponent<BoxCollider> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+
+
+
+
+	void OnTriggerStay(Collider other) {
+		cd = other;
+
+		if (other.tag != "sensor" && other != transform.parent && other.tag != "bullet" && other.gameObject != transform.parent.gameObject ) {
+			
+			bisivr = true;
+			bx.center = nwcntr;
+			bx.size = nwsz;
+		}
+
+
+		if (other.GetComponent<health> () != null) {
+			if (other.GetComponent<health> ().healthf <= 0) {
+				bisivr = false;
+			}
+		}
+		}
+	
+	
+
+	void OnTriggerExit(Collider other) {
+
+
+		if (other.tag != "bullet") {
+			bisivr = false;
+			bx.size = new Vector3 (1, 1, 1);
+			bx.center = new Vector3 (0, 0, 0);
+		}
+
+
+	}
+
+}
